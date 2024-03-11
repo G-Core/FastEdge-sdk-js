b9e645d6bb1bf9082367abedbc42b6f0df990b09
Gordon Farquharson <gordon.farquharson@gcore.com>
2024-03-11 11:08:14 +0000

    added basic github action to run unit tests
    test run of github actions
    
    upgrade githubActions to latest versions
    
    enabled pr-tests to run eslint / filenaming tests

--- build-examples-config.js
+++ build-examples-config.js
@@ -1,6 +1,6 @@
 // todo: import { componentize } from '@fastedge/sdk-js';
 
-import { componentize } from './componentize.js';
+import { componentize } from './componentize';
 
 await componentize('./docs/examples/basic.js', './dist/basic.wasm');
 await componentize('./docs/examples/downstream-fetch.js', './dist/downstream-fetch.wasm');
