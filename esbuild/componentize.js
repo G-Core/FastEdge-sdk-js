3c3dc3f81e837c8be1fe76afa4bff9acc1d44fbd
Gordon Farquharson <gordon.farquharson@gcore.com>
2024-04-10 09:26:09 +0100

    feat: :sparkles: Upgraded npm package for npx usage
    Allow users to run componentize-cli using npx without needing to install anything.
    
    esbuild-shabang injection
    
    esbuild-shabang injection

--- esbuild/componentize.js
+++ esbuild/componentize.js
@@ -1,4 +1,7 @@
 import { build } from "esbuild";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { readFileSync, writeFileSync } from "node:fs";
 
 await build({
   entryPoints: ["./src/componentize.js", "./src/componentize-cli.js"],
@@ -16,3 +19,13 @@ await build({
     "acorn-walk",
   ],
 });
+
+const prependNodeShebangToFile = (relativeFilePath) => {
+  const filePath = fileURLToPath(
+    new URL(path.resolve(process.cwd(), relativeFilePath), import.meta.url),
+  );
+  const content = readFileSync(filePath, "utf8");
+  writeFileSync(filePath, "#!/usr/bin/env node\n\n" + content);
+};
+
+prependNodeShebangToFile("./componentize-cli.js");
