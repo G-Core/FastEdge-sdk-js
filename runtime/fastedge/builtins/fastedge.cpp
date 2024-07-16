7a2f038ade0e09e28680c368251271871a3da9ad
Gordon Farquharson <gordon.farquharson@gcore.com>
2024-07-16 10:22:04 +0100

    broken cpp

--- runtime/fastedge/builtins/fastedge.cpp
+++ runtime/fastedge/builtins/fastedge.cpp
@@ -9,8 +9,7 @@ bool DEBUG_LOGGING_ENABLED = false;
 api::Engine *ENGINE;
 
 static void oom_callback(JSContext *cx, void *data) {
-  fprintf(stderr, "Critical Error: out of memory\n");
-  fflush(stderr);
+  cerr << "Critical Error: out of memory" << endl;
 }
 
 } // namespace
@@ -19,62 +18,18 @@ bool debug_logging_enabled() { return DEBUG_LOGGING_ENABLED; }
 
 namespace fastedge::fastedge {
 
-// const JSErrorFormatString *FastEdgeGetErrorMessage(void *userRef, unsigned errorNumber) {
-//   if (errorNumber > 0 && errorNumber < JSErrNum_Limit) {
-//     return &fastedge_ErrorFormatString[errorNumber];
-//   }
-
-//   return nullptr;
-// }
-
-// namespace {
-
-// bool enableDebugLogging(JSContext *cx, unsigned argc, JS::Value *vp) {
-//   JS::CallArgs args = CallArgsFromVp(argc, vp);
-//   if (!args.requireAtLeast(cx, __func__, 1))
-//     return false;
-//   DEBUG_LOGGING_ENABLED = JS::ToBoolean(args[0]);
-//   args.rval().setUndefined();
-//   return true;
-// }
-
-// } // namespace
-
 JS::PersistentRooted<JSObject *> FastEdge::env;
-
-
-bool FastEdge::now(JSContext *cx, unsigned argc, JS::Value *vp) {
-  JS::CallArgs args = CallArgsFromVp(argc, vp);
-  args.rval().setNumber(JS_Now());
-  return true;
-}
-
-
-
-const JSPropertySpec FastEdge::properties[] = {
-    // JS_PSG("env", env_get, JSPROP_ENUMERATE),
-    // JS_PSGS("baseURL", baseURL_get, baseURL_set, JSPROP_ENUMERATE),
-    // JS_PSGS("defaultBackend", defaultBackend_get, defaultBackend_set, JSPROP_ENUMERATE),
-    // JS_PSGS("allowDynamicBackends", allowDynamicBackends_get, allowDynamicBackends_set,
-    //         JSPROP_ENUMERATE),
-    // JS_PSG("sdkVersion", version_get, JSPROP_ENUMERATE),
-    JS_PS_END};
-
-
+JS::PersistentRooted<JSObject *> FastEdge::fs;
 
 bool FastEdge::getEnv(JSContext* cx, unsigned argc, JS::Value* vp) {
   JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
-  fprintf(stdout, "farq: getEnv() -> invoked\n");
-
   if (!args.requireAtLeast(cx, "getEnv", 1)) {
-    fprintf(stderr, "Error: getEnv() -> requires at least 1 argument\n");
+    cerr << "Error: getEnv() -> requires at least 1 argument" << endl;
     return false;
   }
 
   auto key_chars = core::encode(cx, args[0]);
   auto val_chars = std::getenv(std::string(key_chars).c_str());
-
-  fprintf(stdout, "farq: getEnv() -> %s\n", std::string(val_chars).c_str());
 
   JS::RootedString jsEnvStr(cx, JS_NewStringCopyZ(cx, std::string(val_chars).c_str()));
   args.rval().setString(jsEnvStr);
@@ -124,58 +79,13 @@ bool FastEdge::readFileSync(JSContext *cx, unsigned argc, JS::Value *vp) {
   return true;
 }
 
-
-
-bool FastEdge::includeBytes(JSContext *cx, unsigned argc, JS::Value *vp) {
-  JS::CallArgs args = CallArgsFromVp(argc, vp);
-
-  INIT_ONLY("fastedge.includeBytes");
-  if (!args.requireAtLeast(cx, "fastedge.includeBytes", 1))
-    return false;
-
-  auto path = core::encode(cx, args[0]);
-  if (!path) {
-    return false;
-  }
-
-  FILE *fp = fopen(path.begin(), "r");
-  if (!fp) {
-    fprintf(stdout, "includeBytes() -> Error opening file: %s \n", std::string(path.begin()).c_str());
-    fflush(stdout);
-    JS_ReportErrorUTF8(cx, "Error opening file %s", path.begin());
-    return false;
-  }
-
-  fseek(fp, 0L, SEEK_END);
-  size_t size = ftell(fp);
-  rewind(fp);
-  JS::RootedObject typed_array(cx, JS_NewUint8Array(cx, size));
-  if (!typed_array)
-    return false;
-
-  size_t read_bytes;
-  {
-    JS::AutoCheckCannotGC noGC(cx);
-    bool is_shared;
-    void *buffer = JS_GetArrayBufferViewData(typed_array, &is_shared, noGC);
-    read_bytes = fread(buffer, 1, size, fp);
-  }
-
-  if (read_bytes != size) {
-    JS_ReportErrorUTF8(cx, "Failed to read contents of file %s", path.begin());
-    return false;
-  }
-
-  args.rval().setObject(*typed_array);
-  return true;
-}
-
+const JSPropertySpec FastEdge::properties[] = {
+    JS_PSG("env", getEnv, JSPROP_ENUMERATE),
+    JS_PS_END
+};
 
 bool install(api::Engine *engine) {
   ENGINE = engine;
-
-  bool ENABLE_EXPERIMENTAL_HIGH_RESOLUTION_TIME_METHODS =
-      std::string(std::getenv("ENABLE_EXPERIMENTAL_HIGH_RESOLUTION_TIME_METHODS")) == "1";
 
   JS::SetOutOfMemoryCallback(engine->cx(), oom_callback, nullptr);
 
@@ -188,52 +98,46 @@ bool install(api::Engine *engine) {
     return false;
   }
 
-  JSFunctionSpec nowfn = JS_FN("now", FastEdge::now, 0, JSPROP_ENUMERATE);
-  JSFunctionSpec end = JS_FS_END;
-
   const JSFunctionSpec methods[] = {
       JS_FN("getEnv", FastEdge::getEnv, 1, JSPROP_ENUMERATE),
-      // JS_FN("enableDebugLogging", enableDebugLogging, 1, JSPROP_ENUMERATE),
-      // JS_FN("getGeolocationForIpAddress", Fastly::getGeolocationForIpAddress, 1, JSPROP_ENUMERATE),
-      JS_FN("includeBytes", FastEdge::includeBytes, 1, JSPROP_ENUMERATE),
       JS_FN("readFileSync", FastEdge::readFileSync, 1, JSPROP_ENUMERATE),
-      ENABLE_EXPERIMENTAL_HIGH_RESOLUTION_TIME_METHODS ? nowfn : end, end};
+  };
 
   if (!JS_DefineFunctions(engine->cx(), fastedge, methods) ||
       !JS_DefineProperties(engine->cx(), fastedge, FastEdge::properties)) {
     return false;
   }
 
+/*
+  // Ensure that the fastedge objects are not garbage collected and modules are defined
+  // fastedge:env
+  RootedValue get_env_val(engine->cx());
+  if (!JS_GetProperty(engine->cx(), fastedge, "getEnv", &get_env_val)) {
+    return false;
+  }
+  RootedObject env_builtin(engine->cx(), JS_NewObject(engine->cx(), nullptr));
+  if (!JS_SetProperty(engine->cx(), env_builtin, "getEnv", get_env_val)) {
+    return false;
+  }
+  RootedValue env_builtin_val(engine->cx(), JS::ObjectValue(*env_builtin));
+  if (!engine->define_builtin_module("fastedge::env", env_builtin_val)) {
+    return false;
+  }
 
-  // RootedValue test_val(engine->cx(), JS::StringValue(JS_NewStringCopyZ(engine->cx(), "world")));
-  // // fastly:env
-  // RootedObject test_builtin(engine->cx(), JS_NewObject(engine->cx(), nullptr));
-  // if (!JS_SetProperty(engine->cx(), test_builtin, "hello", test_val)) {
-  //   return false;
-  // }
-  // RootedValue test_builtin_val(engine->cx(), JS::ObjectValue(*test_builtin));
-  // if (!engine->define_builtin_module("fastedge:test", test_builtin_val)) {
-  //   return false;
-  // }
-
-  // fastly:experimental
-  // RootedObject experimental(engine->cx(), JS_NewObject(engine->cx(), nullptr));
-  // RootedValue experimental_val(engine->cx(), JS::ObjectValue(*experimental));
-  // RootedValue include_bytes_val(engine->cx());
-  // if (!JS_GetProperty(engine->cx(), fastedge, "includeBytes", &include_bytes_val)) {
-  //   return false;
-  // }
-  // if (!JS_GetProperty(engine->cx(), fastedge, "readFileSync", &include_bytes_val)) {
-  //   return false;
-  // }
-  // if (!JS_SetProperty(engine->cx(), experimental, "includeBytes", include_bytes_val)) {
-  //   return false;
-  // }
-  // if (!engine->define_builtin_module("fastly:experimental", experimental_val)) {
-  //   return false;
-  // }
-
-
+  // fastedge:fs
+  RootedValue read_file_sync_val(engine->cx());
+  if (!JS_GetProperty(engine->cx(), fastedge, "readFileSync", &read_file_sync_val)) {
+    return false;
+  }
+  RootedObject fs_builtin(engine->cx(), JS_NewObject(engine->cx(), nullptr));
+  if (!JS_SetProperty(engine->cx(), fs_builtin, "readFileSync", read_file_sync_val)) {
+    return false;
+  }
+  RootedValue fs_builtin_val(engine->cx(), JS::ObjectValue(*fs_builtin));
+  if (!engine->define_builtin_module("fastedge::fs", fs_builtin_val)) {
+    return false;
+  }
+*/
   return true;
 }
 
--- runtime/fastedge/builtins/fastedge.h
+++ runtime/fastedge/builtins/fastedge.h
@@ -9,38 +9,28 @@
 #include "../../StarlingMonkey/runtime/encode.h"
 #include "extension-api.h"
 
-
+// Bring the names from the std namespace into the global namespace
+using std::cerr;
+using std::endl;
+using std::string;
 
 namespace fastedge::fastedge {
 
-const JSErrorFormatString *FastEdgeGetErrorMessage(void *userRef, unsigned errorNumber);
-
 class FastEdge : public builtins::BuiltinNoConstructor<FastEdge> {
 private:
-  // TODO(GB): reimplement
   // static bool log(JSContext *cx, unsigned argc, JS::Value *vp);
-
 public:
   static constexpr const char *class_name = "FastEdge";
 
   static JS::PersistentRooted<JSObject *> env;
+  static JS::PersistentRooted<JSObject *> fs;
 
   static const JSPropertySpec properties[];
 
-  static bool now(JSContext *cx, unsigned argc, JS::Value *vp);
-  // TODO(GB): reimplement
-  // static bool dump(JSContext *cx, unsigned argc, JS::Value *vp);
-  // static bool enableDebugLogging(JSContext *cx, unsigned argc, JS::Value *vp);
-  // static bool getLogger(JSContext *cx, unsigned argc, JS::Value *vp);
-
-  static bool includeBytes(JSContext *cx, unsigned argc, JS::Value *vp);
   static bool readFileSync(JSContext *cx, unsigned argc, JS::Value *vp);
   static bool getEnv(JSContext *cx, unsigned argc, JS::Value *vp);
 
 };
-
-JS::Result<std::tuple<JS::UniqueChars, size_t>> convertBodyInit(JSContext *cx,
-                                                                JS::HandleValue bodyInit);
 
 } // namespace fastedge::fastedge
 
