cf6b4aed0c2c9aea0fed84b4bfe08e7c185778f3
Gordon Farquharson <godronus@yahoo.com>
2026-01-13 09:14:26 +0000

    override console.log implementation

---
+++ runtime/fastedge/builtins/console-override.cpp
@@ -0,0 +1,46 @@
+#include "builtin.h"
+
+#include "../../StarlingMonkey/builtins/web/console.h"
+#include <cstdio>
+
+namespace builtins::web::console {
+
+// Override the weak symbol from StarlingMonkey with a strong symbol
+// This implementation will be used instead of the default one
+void builtin_impl_console_log(Console::LogType log_ty, const char *msg) {
+  const char *prefix = nullptr;
+  switch (log_ty) {
+  case Console::LogType::Log:
+    prefix = "stdout_log";
+    break;
+  case Console::LogType::Debug:
+    prefix = "stdout_debug";
+    break;
+  case Console::LogType::Info:
+    prefix = "stdout_info";
+    break;
+  case Console::LogType::Warn:
+    prefix = "stdout_warn";
+    break;
+  case Console::LogType::Error:
+    prefix = "stdout_error";
+    break;
+  default:
+    prefix = "stdout";
+    break;
+  }
+
+  fprintf(stdout, "%s: %s\n", prefix, msg);
+  fflush(stdout);
+}
+
+} // namespace builtins::web::console
+
+namespace fastedge::console_override {
+
+// This builtin doesn't need to install anything, it just overrides the weak symbol
+bool install(api::Engine *engine) {
+  return true;
+}
+
+} // namespace fastedge::console_override
